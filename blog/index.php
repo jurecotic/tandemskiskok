67d2d47f33149d76dc43adcce41a09ec59130a91
Jure Cotič <jc@lost.si>
2020-04-21 18:38:58 +0200

    add blog

---
+++ blog/index.php
@@ -0,0 +1,262 @@
+<?php include('../header.php'); ?>
+    <div class="content">
+        <div class="meta">
+            <div class="seo_title">skok s padalom, tandemski skok, padalska šola | blog - tandemski skok s padalom, padalska šola, vprašanja in odgovori</div>
+            <div class="seo_description">Tandemski padalski skok iz letala. Letališča Lesce, Slovenj Gradec, Portorož.</div>
+            <div class="seo_keywords">skok s padalom, tandemski skok, padalska šola, prosti pad</div>
+            <span id="qode_page_id">153</span>
+            <div class="body_classes">page-template,page-template-blog-masonry,page-template-blog-masonry-php,page,page-id-153,page-parent,ajax_fade,page_not_loaded,smooth_scroll,,wpb-js-composer js-comp-ver-4.12,vc_responsive</div>
+        </div>
+        <div class="content_inner">
+            <div class="title with_breadcrumbs" style="">
+                <div class="title_holder">
+                    <div class="container">
+                        <div class="container_inner clearfix">
+                            <div class="breadcrumb">
+                                <div class="breadcrumbs"><a href="/">Home</a><span class="delimiter">&#47;</span> <a href="/">Blog</a></div>
+                            </div> <span class="subtitle"> vprašanja in zanimive informacije</span></div>
+                    </div>
+                </div>
+            </div>
+            <a id='back_to_top' href='#'>
+                <span class="icon-stack">
+<i class="icon-chevron-up" style=""></i>
+</span>
+            </a>
+            <div class="container">
+                <div class="container_inner clearfix">
+                <div><?php include ('../CuteNews/show_news.php'); ?></div>
+                    <div class="blog_holder masonry">
+                    
+                        <article id="post-539" class="post-539 post type-post status-publish format-standard hentry category-uncategorized">
+                            <div class="post_text">
+                                <div class="post_description">
+                                    <span class="post_infos">
+<span
+class="date">06</span> <span class="month">August</span> | <a href="/category/uncategorized/" rel="category tag">Uncategorized</a> | <a class="post_author" href="/author/aljazm/">aljazm</a>
+                                    </span>
+                                </div>
+                                <h4><a href="/sodelovali-smo-v-slovenskem-drzavnem-rekordu-v-velikih-formacijah/" title="Sodelovali smo v slovenskem državnem rekordu v velikih formacijah">Sodelovali smo v slovenskem državnem rekordu v velikih formacijah</a></h4> 3.8.2015 je v Bovcu 35 padalcev postavilo nov državni rekord v velikih formacijah. Po skoku iz štirih letal so se ujeli v zraku in sestavili v naprej določeno formacijo. Med novimi nosilci državnega rekorda so tudi naši tandem piloti. <span class="post_social">
+<span class="blog_like">
+<a href="#" class="qode-like" id="qode-like-539" title="Like this"><i
+class="icon-heart icon-large"></i><span
+class="qode-like-count">0</span></a>
+                                </span>
+                                </span>
+                            </div>
+                        </article>
+                        <article id="post-512" class="post-512 post type-post status-publish format-standard has-post-thumbnail hentry category-uncategorized tag-snemanje-skoka">
+                            <div class="post_image">
+                                <a href="/ce-ni-na-posnetku-se-ni-zgodilo/" title="Če ni na posnetku, se ni zgodilo">
+                                    <img width="1580" height="840" src="/wp-content/uploads/2014/03/gopro.jpg" class="attachment-full size-full wp-post-image" alt="gopro hero 3+ black" srcset="/wp-content/uploads/2014/03/gopro.jpg 1580w, /wp-content/uploads/2014/03/gopro-300x159.jpg 300w, /wp-content/uploads/2014/03/gopro-1024x544.jpg 1024w" sizes="(max-width: 1580px) 100vw, 1580px" /> </a>
+                            </div>
+                            <div class="post_text">
+                                <div class="post_description">
+                                    <span class="post_infos">
+<span
+class="date">06</span> <span class="month">March</span> | <a href="/category/uncategorized/" rel="category tag">Uncategorized</a> | <a class="post_author" href="/author/aljazm/">aljazm</a>
+                                    </span>
+                                </div>
+                                <h4><a
+href="/ce-ni-na-posnetku-se-ni-zgodilo/" title="Če ni na posnetku, se ni zgodilo">Če ni na posnetku, se ni zgodilo</a></h4> Padalci radi rečemo: "če ni na posnetku, se ni zgodilo". Na področju snemanja tandemskih skokov s padalom v tehniki handycam, smo v Sloveniji pravi pionirji, saj smo prvi to tehnologijo prinesli na slovenski trg že leta 2008. V tistem času so se uporabljale Sonyeve kamere z... <span class="post_social">
+<span
+class="blog_like">
+<a href="#" class="qode-like" id="qode-like-512" title="Like this"><i class="icon-heart icon-large"></i><span class="qode-like-count">5</span></a>
+                                </span>
+                                </span>
+                            </div>
+                        </article>
+                        <article id="post-504" class="post-504 post type-post status-publish format-standard has-post-thumbnail hentry category-uncategorized tag-letalisce-bovec">
+                            <div class="post_image">
+                                <a href="/tandemski-skok-s-padalom-letalisce-bovec/" title="Tandemski skok s padalom letališče Bovec">
+                                    <img width="2592" height="1944" src="/wp-content/uploads/2014/01/GOPR6578.jpg" class="attachment-full size-full wp-post-image" alt="Skok s padalom letališče Bovec" srcset="/wp-content/uploads/2014/01/GOPR6578.jpg 2592w, /wp-content/uploads/2014/01/GOPR6578-300x225.jpg 300w, /wp-content/uploads/2014/01/GOPR6578-1024x768.jpg 1024w" sizes="(max-width: 2592px) 100vw, 2592px" /> </a>
+                            </div>
+                            <div class="post_text">
+                                <div class="post_description">
+                                    <span class="post_infos">
+<span
+class="date">17</span> <span class="month">February</span> | <a href="/category/uncategorized/" rel="category tag">Uncategorized</a> | <a class="post_author" href="/author/aljazm/">aljazm</a>
+                                    </span>
+                                </div>
+                                <h4><a
+href="/tandemski-skok-s-padalom-letalisce-bovec/" title="Tandemski skok s padalom letališče Bovec">Tandemski skok s padalom letališče Bovec</a></h4> Tandemski skoki s padalom na letališču Bovec so prav posebno doživetje. Podobno kot pri skokih s padalom na letališču Lesce je razgled na okoliške hribe prav neverjeten. Padalski skok v bovško dolino pa nekaj posebnega. Na letališču Bovec smo žal samo enkrat letno  (v avgustu),... <span class="post_social">
+<span
+class="blog_like">
+<a
+href="#" class="qode-like" id="qode-like-504" title="Like this"><i
+class="icon-heart icon-large"></i><span
+class="qode-like-count">0</span></a>
+                                </span>
+                                </span>
+                            </div>
+                        </article>
+                        <article id="post-501" class="post-501 post type-post status-publish format-standard hentry category-uncategorized tag-letalise-slovenj-gradec">
+                            <div class="post_text">
+                                <div class="post_description">
+                                    <span class="post_infos">
+<span
+class="date">16</span> <span class="month">February</span> | <a href="/category/uncategorized/" rel="category tag">Uncategorized</a> | <a class="post_author" href="/author/aljazm/">aljazm</a>
+                                    </span>
+                                </div>
+                                <h4><a
+href="/tandemski-skok-s-padalom-letalisce-slovenj-gradec/" title="Tandemski skok s padalom letališče Slovenj Gradec &#8211; trenutno zaprto za skoke s padalom">Tandemski skok s padalom letališče Slovenj Gradec &#8211; trenutno zaprto za skoke s padalom</a></h4> Tandemski skok s padalom na letališču Slovenj Gradec je posebno doživetje. Letališče Slovenj Gradec ima za skoke s padalom najboljše domače letalo Pilatus Porter. Pilatus Porter je turbo propelersko letalo za 10 padalcev, ki brez težav dosežen najvišjo višino iz katere izvajamo tandemske skoke 4000m.... <span class="post_social">
+<span
+class="blog_like">
+<a
+href="#" class="qode-like" id="qode-like-501" title="Like this"><i
+class="icon-heart icon-large"></i><span
+class="qode-like-count">0</span></a>
+                                </span>
+                                </span>
+                            </div>
+                        </article>
+                        <article id="post-491" class="post-491 post type-post status-publish format-standard has-post-thumbnail hentry category-uncategorized tag-letalisce-portoroz">
+                            <div class="post_image">
+                                <a href="/tandemski-skok-s-padalom-letalisce-portoroz/" title="Tandemski skok s padalom letališče Portorož">
+                                    <img width="1000" height="750" src="/wp-content/uploads/2014/02/GOPR0928-e1392406015535.jpg" class="attachment-full size-full wp-post-image" alt="skok s padalom" /> </a>
+                            </div>
+                            <div class="post_text">
+                                <div class="post_description">
+                                    <span class="post_infos">
+<span
+class="date">14</span> <span class="month">February</span> | <a href="/category/uncategorized/" rel="category tag">Uncategorized</a> | <a class="post_author" href="/author/aljazm/">aljazm</a>
+                                    </span>
+                                </div>
+                                <h4><a
+href="/tandemski-skok-s-padalom-letalisce-portoroz/" title="Tandemski skok s padalom letališče Portorož">Tandemski skok s padalom letališče Portorož</a></h4> Letališče Portorož je za tandemski skok s padalom eno bolj popularnih letališč. Pohvali se lahko s čudovitim razgledom na slovensko in hrvaško morje. V sezoni 2013 smo za tandemske skoke s padalom v Portorožu letalo Piper Seneca, letalo katero lahko na višino pelje 5 padalcev. Prikaži večji... <span class="post_social">
+<span
+class="blog_like">
+<a
+href="#" class="qode-like" id="qode-like-491" title="Like this"><i
+class="icon-heart icon-large"></i><span
+class="qode-like-count">20</span></a>
+                                </span>
+                                </span>
+                            </div>
+                        </article>
+                        <article id="post-485" class="post-485 post type-post status-publish format-standard has-post-thumbnail hentry category-uncategorized tag-letalisce-lesce">
+                            <div class="post_image">
+                                <a href="/tandemski-skok-s-padalom-letalisce-lesce/" title="Tandemski skok s padalom letališče Lesce &#8211; trenutno zaprto za skoke s padalom">
+                                    <img width="1200" height="900" src="/wp-content/uploads/2014/02/skok_s_padalom_lesce-e1392313956927.jpg" class="attachment-full size-full wp-post-image" alt="Tandemski skok s padalom Lesce" /> </a>
+                            </div>
+                            <div class="post_text">
+                                <div class="post_description">
+                                    <span class="post_infos">
+<span
+class="date">13</span> <span class="month">February</span> | <a href="/category/uncategorized/" rel="category tag">Uncategorized</a> | <a class="post_author" href="/author/aljazm/">aljazm</a>
+                                    </span>
+                                </div>
+                                <h4><a
+href="/tandemski-skok-s-padalom-letalisce-lesce/" title="Tandemski skok s padalom letališče Lesce &#8211; trenutno zaprto za skoke s padalom">Tandemski skok s padalom letališče Lesce &#8211; trenutno zaprto za skoke s padalom</a></h4> Letališče Lesce je naše domače letališče in eno izmed naših najljubših letališč za skoke s padalom. Ne samo zaradi centralne lege, ampak tudi zaradi panorame in postavitve samega letališča.  Po vzletu letalo v večini primerov zavije in nam ponuja vrhunski panoramski leti le nekaj 100... <span class="post_social">
+<span
+class="blog_like">
+<a
+href="#" class="qode-like" id="qode-like-485" title="Like this"><i
+class="icon-heart icon-large"></i><span
+class="qode-like-count">16</span></a>
+                                </span>
+                                </span>
+                            </div>
+                        </article>
+                        <article id="post-449" class="post-449 post type-post status-publish format-quote hentry category-uncategorized post_format-post-format-quote">
+                            <div class="post_text">
+                                <div class="post_text_holder">
+                                    <i class="qoute_mark icon-quote-right pull-left"></i>
+                                    <div class="post_description">
+                                        <span class="post_infos">
+<span
+class="date">04</span> <span class="month">January</span> | <a href="/category/uncategorized/" rel="category tag">Uncategorized</a> | <a class="post_author" href="/author/aljazm/">aljazm</a>
+                                        </span>
+                                    </div>
+                                    <div class="post_title">
+                                        <p><a href="/the-moment-you-doubt-whether-you-can-fly-you-cease-for-ever-to-be-able-to-do-it-j-m-barrie-peter-pan/" title="J.M. Barrie, Peter Pan">“The moment you doubt whether you can fly, you cease for ever to be able to do it.”</a></p>
+                                        <span class="quote_author">&mdash; J.M. Barrie, Peter Pan</span></div>
+                                    <span class="post_social">
+<span
+class="blog_like">
+<a
+href="#" class="qode-like" id="qode-like-449" title="Like this"><i
+class="icon-heart icon-large"></i><span
+class="qode-like-count">0</span></a>
+                                    </span>
+                                    </span>
+                                </div>
+                            </div>
+                        </article>
+                        <article id="post-174" class="post-174 post type-post status-publish format-standard hentry category-faq">
+                            <div class="post_text">
+                                <div class="post_description">
+                                    <span class="post_infos">
+<span
+class="date">25</span> <span class="month">December</span> | <a href="/category/faq/" rel="category tag">FAQ</a> | <a class="post_author" href="/author/aljazm/">aljazm</a>
+                                    </span>
+                                </div>
+                                <h4><a
+href="/kdo-lahko-skoci-s-tandemom/" title="Kdo lahko skoči s tandemom?">Kdo lahko skoči s tandemom?</a></h4> V Sloveniji ne veljajo omejitve glede starosti oseb, ki lahko skočijo s tandemom. Morate biti večji od 135 cm in lažji od 110kg in seveda primernega zdravja. V naši padalski tandemski šoli tudi ne dovolimo skakanja mlajšim od 10 let, saj smo mnenja, da pred... <span class="post_social">
+<span
+class="blog_like">
+<a
+href="#" class="qode-like" id="qode-like-174" title="Like this"><i
+class="icon-heart icon-large"></i><span
+class="qode-like-count">10</span></a>
+                                </span>
+                                </span>
+                            </div>
+                        </article>
+                        <article id="post-172" class="post-172 post type-post status-publish format-standard hentry category-faq">
+                            <div class="post_text">
+                                <div class="post_description">
+                                    <span class="post_infos">
+<span
+class="date">25</span> <span class="month">December</span> | <a href="/category/faq/" rel="category tag">FAQ</a> | <a class="post_author" href="/author/aljazm/">aljazm</a>
+                                    </span>
+                                </div>
+                                <h4><a
+href="/zakaj-ljudje-skacejo-iz-letalnih-naprav-in-se-uvarjajo-s-padalstvom/" title="Zakaj ljudje skačejo iz letalnih naprav in se uvarjajo s padalstvom?">Zakaj ljudje skačejo iz letalnih naprav in se uvarjajo s padalstvom?</a></h4> Verjetno obstaja toliko razlogov, kot je ljudi, ki se ukvarjajo s padalstvom ali pa so že skočili v tandemu. Za nekatere je to življenska želja, za druge premagovanje ovir in novi izzivi in za nekatere možnost, da preizkusijo "nekaj ekstremnega". <span class="post_social">
+<span
+class="blog_like">
+<a
+href="#" class="qode-like" id="qode-like-172" title="Like this"><i
+class="icon-heart icon-large"></i><span
+class="qode-like-count">3</span></a>
+                                </span>
+                                </span>
+                            </div>
+                        </article>
+                        <article id="post-170" class="post-170 post type-post status-publish format-standard hentry category-faq">
+                            <div class="post_text">
+                                <div class="post_description">
+                                    <span class="post_infos">
+<span
+class="date">25</span> <span class="month">December</span> | <a href="/category/faq/" rel="category tag">FAQ</a> | <a class="post_author" href="/author/aljazm/">aljazm</a>
+                                    </span>
+                                </div>
+                                <h4><a
+href="/kaj-je-tandemski-padalski-skok/" title="Kaj je tandemski padalski skok?">Kaj je tandemski padalski skok?</a></h4> Ena lepših življenskih izkušenj, skok iz letala in prosti pad, jadranje s padalom, ...  je postala večini dosegljiva, predvsem zaradi tandemskih padalskih skokov. Varno pripeti na svojega inštruktorja, ki vas bo vodil čez celotno doživetje, se boste lahko sprostili, saj boste vedeli, da boste skočili z enim... <span class="post_social">
+<span
+class="blog_like">
+<a
+href="#" class="qode-like" id="qode-like-170" title="Like this"><i
+class="icon-heart icon-large"></i><span
+class="qode-like-count">36</span></a>
+                                </span>
+                                </span>
+                            </div>
+                        </article>
+                    </div>
+                    <div class='pagination'>
+                        <ul>
+                            <li class='prev'><a href='/blog/'><i
+class='icon-chevron-left'></i></a></li>
+                            <li class='active'><span>1</span></li>
+                            <li><a href='/blog/page/2/' class='inactive'>2</a></li>
+                            <li class='next'><a href="/blog/page/2/"><i
+class='icon-chevron-right'></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="content_bottom"></div>
+        </div>
+    </div>
+    <?php include('../footer.php'); ?>
