c82b61344507f55d16a69a5f4b3491de37c18bd9
Jure Cotič <jc@lost.si>
2020-04-21 19:58:55 +0200

    add changes

---
+++ CuteNews/cdata/users/47.php
@@ -0,0 +1,2 @@
+<?php die('Direct call - access denied'); ?>
+YToxOntzOjU6ImVtYWlsIjthOjE6e3M6MTA6ImpjQGxvc3Quc2kiO3M6NjoiY290aWNqIjt9fQ==
---
+++ CuteNews/cdata/users/c6.php
@@ -0,0 +1,2 @@
+<?php die('Direct call - access denied'); ?>
+YToxOntzOjI6ImlkIjthOjE6e2k6MTU4NzQ4ODEwMjtzOjY6ImNvdGljaiI7fX0=
